e9dd4a741ce378d9da17852754b9201d96383d6c
Rameela-Mushtaq <rameelamushtaq2gmail.com>
2025-01-28 15:58:17 +0500

    resolve slash issue

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 // export const backend_url = 'http://localhost:5000';
-export const backend_url = 'https://cyber-backend-eta.vercel.app/';
+export const backend_url = 'https://cyber-backend-eta.vercel.app';
